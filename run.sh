e2f5d441f5060b55685c7bf4f5971917d1af631b
Vassilis Zafeiris <bzafiris@gmail.com>
2018-03-23 17:07:42 +0200

    run script update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./RefactoringMiner/bin/RefactoringMiner -a $1 master
+./RefactoringMiner/bin/RefactoringMiner -a $1 $2
 cp $1/all_refactorings_master.csv ~/workspace-eclipse/workspace-rminer/rminer-results/$1.csv
